6f3165aae1ac601a27e77f98d72bfba9b10477a6
Yevhenii <grimm.krl@gmail.com>
2024-10-19 02:03:40 +0300

    test

---
+++ js/hw.js
@@ -0,0 +1,8 @@
+// let text = "5text"
+// let number = 5
+// let wdf3 = text+number+number
+// console.log(wdf3)
+
+
+let arrayTest = [1,2,3,"string", {"objectName":"Eugene"},4]
+console.log(arrayTest)
